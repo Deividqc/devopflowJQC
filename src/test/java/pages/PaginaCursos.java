47ea9ab6819e7ffefe267a1512a621209895ee4e
deividqc <jesusdavidquispecasas@gmail.com>
2026-03-20 01:10:46 -0500

    Fixes

--- src/test/java/pages/PaginaCursos.java
+++ src/test/java/pages/PaginaCursos.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class PaginaCursos extends BasePage {
 
     private String IntroducionTestingLink="//a[normalize-space()='Introducción al Testing de Software' and @href]";
